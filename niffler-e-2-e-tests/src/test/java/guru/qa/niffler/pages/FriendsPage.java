454ab535848bb8e65479b609e59f20b41ccd5c2c
Alena Meteneva <alenameteneva@gmail.com>
2024-01-19 10:03:22 -0800

    pull request correction

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/pages/FriendsPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/pages/FriendsPage.java
@@ -3,20 +3,22 @@ package guru.qa.niffler.pages;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class FriendsPage {
     private final SelenideElement friendsTable = $(".table.abstract-table tbody");
     @Step("Найти в таблице друзей запись")
-    public boolean findRecordInTableByText(String text) {
-        return friendsTable
-                .$$("tr").find(text(text)).isDisplayed();
+    public void findRecordInTableByText(String text) {
+        friendsTable
+                .$$("tr").find(text(text)).shouldBe(visible);
     }
 
     @Step("Посчитать количество строк в таблице друзей")
-    public int countFriendsListSize() {
-        return friendsTable
-                .$$("tr").size();
+    public void countFriendsListSize() {
+        friendsTable
+                .$$("tr").shouldHave(size(1));
     }
 }
