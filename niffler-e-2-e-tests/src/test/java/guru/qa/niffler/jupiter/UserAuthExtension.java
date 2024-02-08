2e44d9758ccc4924bf4938f5a8c09282e36cd023
Alena Meteneva <alenameteneva@gmail.com>
2024-02-08 09:59:50 -0800

    hw7 fix

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/UserAuthExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/UserAuthExtension.java
@@ -7,18 +7,19 @@ import guru.qa.niffler.db.repository.UserRepositoryJdbc;
 import org.junit.jupiter.api.extension.*;
 import org.junit.platform.commons.support.AnnotationSupport;
 
-import java.util.Arrays;
-import java.util.Optional;
+import java.util.*;
 
 public class UserAuthExtension implements BeforeEachCallback, AfterEachCallback, ParameterResolver {
     public static final ExtensionContext.Namespace NAMESPACE
             = ExtensionContext.Namespace.create(UserAuthExtension.class);
     private final UserRepository userRepository = new UserRepositoryJdbc();
-    private UserAuthEntity userAuth;
-    private UserEntity user;
+    private final Faker faker  = new Faker();
+    static String userAuthDataKey = "userAuthDataKey";
+    static String userDataKey = "userDataKey";
+
 
     @Override
-    public void beforeEach(ExtensionContext extensionContext) throws Exception {
+    public void beforeEach(ExtensionContext extensionContext) {
         Optional<DbUser> userDb = AnnotationSupport.findAnnotation(
                 extensionContext.getRequiredTestMethod(),
                 DbUser.class);
@@ -26,14 +27,16 @@ public class UserAuthExtension implements BeforeEachCallback, AfterEachCallback,
         if (userDb.isPresent()) {
             String username;
             String password;
+            Map<String, Object> usersData = new HashMap<>();
+
             if (!userDb.get().username().isEmpty() && !userDb.get().password().isEmpty()) {
                 username = userDb.get().username();
                 password = userDb.get().password();
             } else {
-                username = new Faker().funnyName().name();
-                password = new Faker().internet().password();
+                username = faker.funnyName().name();
+                password = faker.internet().password();
             }
-            userAuth = UserAuthEntity.builder()
+           UserAuthEntity userAuth = UserAuthEntity.builder()
                     .username(username)
                     .password(password)
                     .enabled(true)
@@ -47,21 +50,28 @@ public class UserAuthExtension implements BeforeEachCallback, AfterEachCallback,
                                 return ae;
                             }).toList())
                     .build();
-            user = UserEntity.builder()
+            UserEntity user = UserEntity.builder()
                     .username(username)
                     .currency(CurrencyValues.RUB)
                     .build();
-            userRepository.createInAuth(userAuth);
-            userRepository.createInUserData(user);
+            UUID userAuthId = userRepository.createInAuth(userAuth).getId();
+            userAuth.setId(userAuthId);
+            UUID userDataId = userRepository.createInUserData(user).getId();
+            user.setId(userDataId);
+
+            usersData.put(userAuthDataKey, userAuth);
+            usersData.put(userDataKey, user);
+
             extensionContext.getStore(NAMESPACE)
-                    .put("userAuth", userAuth);
+                    .put(extensionContext.getUniqueId(), usersData);
         }
     }
 
     @Override
-    public void afterEach(ExtensionContext extensionContext) throws Exception {
-        userRepository.deleteInAuthById(userAuth.getId());
-        userRepository.deleteInUserDataById(user.getId());
+    public void afterEach(ExtensionContext extensionContext) {
+        Map usersMap = extensionContext.getStore(NAMESPACE).get(extensionContext.getUniqueId(), Map.class);
+        userRepository.deleteInAuthById(((UserAuthEntity) usersMap.get(userAuthDataKey)).getId());
+        userRepository.deleteInUserDataById(((UserEntity) usersMap.get(userDataKey)).getId());
     }
 
 
@@ -74,7 +84,7 @@ public class UserAuthExtension implements BeforeEachCallback, AfterEachCallback,
 
     @Override
     public UserAuthEntity resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-        return extensionContext.getStore(UserAuthExtension.NAMESPACE)
-                .get("userAuth", UserAuthEntity.class);
+        Map users = extensionContext.getStore(NAMESPACE).get(extensionContext.getUniqueId(), Map.class);
+        return (UserAuthEntity) users.get(userAuthDataKey);
     }
 }
